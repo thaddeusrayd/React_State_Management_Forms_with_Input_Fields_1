7550fc8b07a36e73bfde187431144c80ea49406f
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 11:02:50 -0500

    add handlers to form

--- src/DogForm.js
+++ src/DogForm.js
@@ -1,22 +1,51 @@
+import { use } from "marked";
 import React, { useState } from "react";
 
 function DogForm() {
-  const handleSubmit = () => console.log("yo yoooo");
+  const [name, setName] = useState("");
+  const handleNameChange = (event) => setName(event.target.value);
+  const [breed, setBreed] = useState("");
+  const handleBreedChange = (event) => setBreed(event.target.value);
+  const [age, setAge] = useState("");
+  const handleAgeChange = (event) => setAge(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">
         Name:
-        <input id="name" type="text" name="name" />
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleNameChange}
+          value={name}
+        />
       </label>
       <label htmlFor="breed">
         Breed:
-        <input id="breed" type="text" name="breed" />
+        <input
+          id="breed"
+          type="text"
+          name="breed"
+          onChange={handleBreedChange}
+          value={name}
+        />
       </label>
       <label htmlFor="age">
         Age:
-        <input id="age" type="text" name="age" />
+        <input
+          id="age"
+          type="text"
+          name="age"
+          onChange={handleAgeChange}
+          value={age}
+        />
       </label>
+      <button type="submit">Submit</button>
     </form>
   );
 }
