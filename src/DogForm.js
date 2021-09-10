075938b1bbed561f0d7aafd22d9df3a2858cafa9
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 10:50:41 -0500

    create form and add submit functionality

--- src/DogForm.js
+++ src/DogForm.js
@@ -1,5 +1,24 @@
 import React, { useState } from "react";
 
-function DogForm() {}
+function DogForm() {
+  const handleSubmit = () => console.log("yo yoooo");
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name:
+        <input id="name" type="text" name="name" />
+      </label>
+      <label htmlFor="breed">
+        Breed:
+        <input id="breed" type="text" name="breed" />
+      </label>
+      <label htmlFor="age">
+        Age:
+        <input id="age" type="text" name="age" />
+      </label>
+    </form>
+  );
+}
 
 export default DogForm;
